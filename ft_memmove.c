f3443b5c00667d33ea51b4aba5dcba14c1c80a86
dasilvacontin <daviddasilvacontin@gmail.com>
2014-11-05 16:29:00 +0100

    done ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,18 +6,13 @@
 /*   By: dda-silv <dda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 18:00:25 by dda-silv          #+#    #+#             */
-/*   Updated: 2014/11/04 18:12:07 by dda-silv         ###   ########.fr       */
+/*   Updated: 2014/11/05 16:12:01 by dda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+void  *ft_memmove(void *dst, const void *src, size_t len)
 {
-	byte	*str;
-
-	if (!len || !dst)
-		return (dst);
-	str = (byte *) dst;
-
+            
 }
