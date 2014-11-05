58e50284b5b558b610648880b08400de3709dd0d
dasilvacontin <daviddasilvacontin@gmail.com>
2014-11-05 16:32:16 +0100

    fixed ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: dda-silv <dda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 17:34:49 by dda-silv          #+#    #+#             */
-/*   Updated: 2014/11/05 16:26:33 by dda-silv         ###   ########.fr       */
+/*   Updated: 2014/11/05 16:30:56 by dda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
+	void	*ret;
+
 	if (!dst || !src)
 		return (dst);
+	ret = dst;
 	while (n--)
 	{
 		*(byte *)dst++ = *(byte *)src++;
 	}
-	return (dst);
+	return (ret);
 }
