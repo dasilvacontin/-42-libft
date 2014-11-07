54e2f8df16c9195f46d70d4901bedeaa8cef877c
dasilvacontin <daviddasilvacontin@gmail.com>
2014-11-07 18:00:24 +0100

    done atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: dda-silv <dda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/07 17:01:32 by dda-silv          #+#    #+#             */
-/*   Updated: 2014/11/07 17:29:30 by dda-silv         ###   ########.fr       */
+/*   Updated: 2014/11/07 17:57:32 by dda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,22 @@ int	ft_atoi(const char *str)
 	int	sign;
 
 	sign = 1;
-	total = ft_strlen((char *)str);
+	total = 0;
+	if (*str < '0' || '9' < *str)
+	{
+		if (*str == '-')
+			sign = -1;
+		else if (*str != '+')
+			return (0);
+		str++;
+	}
+	while (*str)
+	{
+		if ('0' <= *str && *str <= '9')
+			total = total * 10 + *str - '0';
+		else
+			break;
+		++str;
+	}
 	return (total * sign);
 }
