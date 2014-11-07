f578ad86de2376b24ca16319f5100509e96e2c6a
dasilvacontin <daviddasilvacontin@gmail.com>
2014-11-07 12:49:52 +0100

    done strncpy, fixed strcpy not copying \0

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -1,23 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dda-silv <dda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/11/07 12:19:36 by dda-silv          #+#    #+#             */
-/*   Updated: 2014/11/07 12:43:26 by dda-silv         ###   ########.fr       */
+/*   Created: 2014/11/07 12:32:56 by dda-silv          #+#    #+#             */
+/*   Updated: 2014/11/07 12:49:23 by dda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcpy(char *dst, char *src)
+#include "libft.h"
+
+char	*ft_strncpy(char *dst, char *src, size_t n)
 {
 	char	*ret;
 
-	if (!dst || !src)
+	if (!dst || !n)
 		return (dst);
-	while (*src)
+	if (!ret)
+		return (ft_bzero(dst, n));
+	while (n-- && *src)
 		*dst++ = *src++;
 	*dst = *src;
+	ft_bzero(dst, n);
 	return (ret);
 }
