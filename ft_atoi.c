6b3724b915ae015cd0ccb95328a3f9fe559d4b54
dasilvacontin <daviddasilvacontin@gmail.com>
2014-11-26 16:40:16 +0100

    fixing

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: dda-silv <dda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/07 17:01:32 by dda-silv          #+#    #+#             */
-/*   Updated: 2014/11/08 12:29:04 by dda-silv         ###   ########.fr       */
+/*   Updated: 2014/11/26 16:21:33 by dda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,6 @@ int	ft_atoi(const char *str)
 		str++;
 	}
 	while (ft_isdigit(*str))
-		total = total * 10 + *str++ - '0';
-	return (total * sign);
+		total = total * 10 + (*str++ - '0') * sign;
+	return (total);
 }
--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: dda-silv <dda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/07 12:19:36 by dda-silv          #+#    #+#             */
-/*   Updated: 2014/11/08 19:29:00 by dda-silv         ###   ########.fr       */
+/*   Updated: 2014/11/26 16:25:11 by dda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@ char	*ft_strcpy(char *dst, const char *src)
 {
 	char	*ret;
 
+	if (!dst || !src)
+		return (dst);
 	ret = (char *)dst;
 	while (*src)
 		*dst++ = *src++;
